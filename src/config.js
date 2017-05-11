2ae59cb3c54c8165d5f1d0d9012d089c5f991cce
Tiago Marques <tiago.map.marques@gmail.com>
2017-05-11 14:11:13 +0100

    Added config helper functions

--- src/config.js
+++ src/config.js
@@ -6,6 +6,10 @@ const config = {
   INIT_FUNCTION: 'init',
 };
 
-const getConfig = newConfig => Object.assign({}, config, newConfig);
+const buildHelpers = newConfig => Object.assign({}, newConfig, {
+  getActionsName: name => `${newConfig.ACTIONS_PREFIX}${name}${newConfig.ACTIONS_SUFFIX}`,
+})
+
+const getConfig = newConfig => buildHelpers(Object.assign({}, config, newConfig));
 
 module.exports = getConfig;
--- src/connect.js
+++ src/connect.js
@@ -3,7 +3,11 @@ const reduxConnect = require('react-redux').connect;
 const actionSelector = require('./actions');
 const reducerSelector = require('./reducers');
 
-const connect = (actions, configuration) => (...args) =>
-  reduxConnect(reducerSelector(...args), actionSelector(actions, configuration)(...args));
+const connect = (actions, configuration) => (...args) => reduxConnect(
+  reducerSelector(...args),
+  actionSelector(Object.keys(actions).reduce((accumulator, key) => Object.assign({}, accumulator, {
+    [configuration.getActionsName(key)]: actions[key],
+  }), {}), configuration)(...args)
+);
 
 module.exports = connect;
